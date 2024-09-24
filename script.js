654e052a4065c909cc49fe4908223ea75dca3af9
Ubuntu <ubuntu@ip-172-31-2-159.eu-central-1.compute.internal>
2024-09-24 12:56:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener(DOMContentLoaded, function() {fetch(/api/posts).then(response => response.json()).then(posts => {const postsDiv = document.getElementById(posts);posts.forEach(post => {const postElement = document.createElement(div);postElement.innerHTML = `<h2>${post.title}</h2><p>${post.content}</p>`;postsDiv.appendChild(postElement);});});});
